21f595b3b12d15f57311cfb12cea159d0e5ae3f7
infotech <infoconwayy@gmail.com>
2026-05-28 12:07:24 +0530

    added url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://slategrey-worm-160018.hostingersite.com";
+export const BASE_URL = "https://lightcyan-woodpecker-502045.hostingersite.com"
+// "https://slategrey-worm-160018.hostingersite.com";
 // export const BASE_URL = "http://localhost:3001";
